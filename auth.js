6e4b9ee95ab56b4f30bc533233ee49af185a80db
ipjrushromania <armancostelus@gmail.com>
2026-07-27 23:15:33 +0300

    Refactor auth.js for Discord authentication
    Removed Firebase authentication functions and added a login event listener for Discord authentication.

--- auth.js
+++ auth.js
@@ -1,55 +1,5 @@
-import { auth } from "./firebase.js";
+document.getElementById("login").addEventListener("click", () => {
 
-import {
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+alert("În Partea 2 vom conecta autentificarea cu Discord.");
 
-window.register = async function(){
-
-const email=document.getElementById("email").value;
-const password=document.getElementById("password").value;
-
-try{
-
-await createUserWithEmailAndPassword(auth,email,password);
-
-alert("Cont creat cu succes!");
-
-location.href="login.html";
-
-}catch(err){
-
-alert(err.message);
-
-}
-
-}
-
-window.login = async function(){
-
-const email=document.getElementById("email").value;
-const password=document.getElementById("password").value;
-
-try{
-
-await signInWithEmailAndPassword(auth,email,password);
-
-location.href="dashboard.html";
-
-}catch(err){
-
-alert(err.message);
-
-}
-
-}
-
-window.logout = async function(){
-
-await signOut(auth);
-
-location.href="login.html";
-
-}
+});
